31a0dbddedd745401e5e5622ed383812efd8c40d
Dominik Urzeniczok <dominik-urzeniczok@wp.pl>
2025-09-18 22:53:06 +0000

    Add Facebook link and opening hours to JSON-LD

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,54 +47,78 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {/* JSON-LD dane strukturalne */}
         <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              "@context": "https://schema.org",
-              "@type": "Electrician",
-              "name": "Rafbakki slf.",
-              "url": "https://www.rafbakki.is",
-              "logo": "https://www.rafbakki.is/logo1.png",
-              "image": "https://www.rafbakki.is/logo1.png",
-              "areaServed": [
-                {
-                  "@type": "Place",
-                  "address": {
-                    "@type": "PostalAddress",
-                    "addressLocality": "Stykkishólmur",
-                    "postalCode": "340",
-                    "addressCountry": "IS",
-                  },
-                },
-                {
-                  "@type": "Place",
-                  "address": {
-                    "@type": "PostalAddress",
-                    "addressLocality": "Hveragerði",
-                    "postalCode": "810",
-                    "addressCountry": "IS",
-                  },
-                },
-              ],
-              "contactPoint": [
-                {
-                  "@type": "ContactPoint",
-                  "telephone": "+354 848 0153",
-                  "contactType": "þjónustuver",
-                  "email": "geiri@rafbakki.is",
-                },
-                {
-                  "@type": "ContactPoint",
-                  "telephone": "+354 765 5233",
-                  "contactType": "þjónustuver",
-                  "email": "dominik@rafbakki.is",
-                },
-              ],
-              "description":
-                "Rafbakki slf. býður upp á raflagnir, sjálfvirkni fyrir heimili og fyrirtæki, uppsetningu á LAN/Wi-Fi netum, öryggiskerfi, vöktun og snjallheimila lausnir.",
-            }),
-          }}
-        />
+  type="application/ld+json"
+  dangerouslySetInnerHTML={{
+    __html: JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "Electrician",
+      "@id": "https://www.rafbakki.is/#organization",
+      "name": "Rafbakki slf.",
+      "url": "https://www.rafbakki.is",
+      "logo": "https://www.rafbakki.is/logo1.png",
+      "image": "https://www.rafbakki.is/logo1.png",
+      "address": {
+        "@type": "PostalAddress",
+        "addressLocality": "Stykkishólmur",
+        "postalCode": "340",
+        "addressCountry": "IS"
+      },
+      "areaServed": [
+        {
+          "@type": "Place",
+          "address": {
+            "@type": "PostalAddress",
+            "addressLocality": "Stykkishólmur",
+            "postalCode": "340",
+            "addressCountry": "IS"
+          }
+        },
+        {
+          "@type": "Place",
+          "address": {
+            "@type": "PostalAddress",
+            "addressLocality": "Hveragerði",
+            "postalCode": "810",
+            "addressCountry": "IS"
+          }
+        }
+      ],
+      "openingHoursSpecification": [
+        {
+          "@type": "OpeningHoursSpecification",
+          "dayOfWeek": [
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday"
+          ],
+          "opens": "08:00",
+          "closes": "16:00"
+        }
+      ],
+      "contactPoint": [
+        {
+          "@type": "ContactPoint",
+          "telephone": "+354 848 0153",
+          "contactType": "þjónustuver",
+          "email": "geiri@rafbakki.is"
+        },
+        {
+          "@type": "ContactPoint",
+          "telephone": "+354 765 5233",
+          "contactType": "þjónustuver",
+          "email": "dominik@rafbakki.is"
+        }
+      ],
+      "sameAs": [
+        "https://www.facebook.com/profile.php?id=100089486814094"
+      ],
+      "description":
+        "Rafbakki slf. býður upp á raflagnir, sjálfvirkni fyrir heimili og fyrirtæki, uppsetningu á LAN/Wi-Fi netum, öryggiskerfi, vöktun og snjallheimila lausnir."
+    }),
+  }}
+/>
         {children}
       </body>
     </html>
