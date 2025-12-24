9949d33cc0e3796345f6e23949563cf524bb4073
Dominik Urzeniczok <dominik-urzeniczok@wp.pl>
2025-12-24 12:36:34 +0000

    Christmas hero video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,19 +134,24 @@ export default function Page() {
               </div>
             </div>
 
-            <div className="relative aspect-[12/16] max-h-[600px] mx-auto rounded-3xl overflow-hidden border border-white/20"
-     style={{ boxShadow: `0 0 60px ${brand.neonViolet}22, inset 0 0 60px ${brand.primary}15` }}>
-  
-  <iframe
-    src="https://www.youtube.com/embed/h8A-Thn1iGU?autoplay=1&mute=1&loop=1&controls=0&playlist=h8A-Thn1iGU&modestbranding=1&rel=0&showinfo=0"
-    title="Rafbakki Showreel"
-    allow="autoplay; encrypted-media; fullscreen"
+<div
+  className="relative aspect-[12/16] max-h-[600px] mx-auto rounded-3xl overflow-hidden border border-white/20"
+  style={{ boxShadow: `0 0 60px ${brand.neonViolet}22, inset 0 0 60px ${brand.primary}15` }}
+>
+  <video
+    autoPlay
+    muted
+    loop
+    playsInline
+    preload="metadata"
     className="w-full h-full object-cover"
-  ></iframe>
+  >
+    <source src="/christmas.mp4" type="video/mp4" />
+  </video>
 
-  {/* Nakładka blokująca kliknięcia */}
-  <div className="absolute inset-0" style={{ pointerEvents: "none" }}></div>
+  <div className="pointer-events-none absolute inset-0 bg-black/10"></div>
 </div>
+
 
 
           </div>
